a2d91f8938c02f67643cb7655eb8a45a5301df68
kayjei <kay.lidenalv@gmail.com>
2019-10-24 23:04:58 +0200

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,1 +1,46 @@
+"""Parameter file for Adax integration"""
 
+def set_param(api_call):
+    switcher= {
+    "account_id": '',
+    "appVersion": '',
+    "device": '',
+    "os": '',
+    "timeOffset": '',
+    "timeZone": '',
+    "zone_signature": '',
+    "heat_signature": '',
+    0: '', #Set as HVAC_OFF
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: '',
+    10:'',
+    11: '',
+    12: '',
+    13: '',
+    14: '',
+    15: '',
+    16: '',
+    17: '',
+    18: '',
+    19: '',
+    20: '',
+    21: '',
+    22: '',
+    23: '',
+    24: '',
+    25: '',
+    26: '',
+    27: '',
+    28: '',
+    29: '',
+    30: '',
+    31: '',
+    32: '',
+    33: '',
+    34: '',
+    35: ''
+    }
+    return switcher.get(api_call,"Key missing")
